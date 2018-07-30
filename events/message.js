41af36ee92b733de03e5754d7a26aa5326ef3dfb
Kevin <kaleman960@gmail.com>
2018-07-30 15:57:42 -0600

    Working on dispatcher

--- events/message.js
+++ events/message.js
@@ -1,19 +1,93 @@
-const axios = require('axios');
-const { bot_token } = process.env;
+const register = require('./register');
+const limits = require('./limits');
+const purchase = require('./purchase');
+const products = require('./products');
+const debt = require('./debt');
 
 const answerMessage = (body) => {
-    console.log(body);
-    const { channel } = body.event;
-    const params = {
-        token: bot_token,
-        channel,
-        text: 'Hello!'
-    };
-    axios.get(`https://slack.com/api/chat.postMessage`, {
-        params,
-    }).then((response) => {
-        console.log(response);
-    })
+    const { text } = body;
+    const { channel, user } = body.event;
+
+    let data;
+    switch(text) {
+        case /^register/.test(text): {
+            data = {
+                username: body.event.user,
+                password: text.split(':')[2],
+                channel,
+                user,
+            }
+            return register(data);
+        }
+        case text.startsWith('show'): {
+            data = {
+                show: text.match(/my|global|\<@\w{9}\>/),
+                channel,
+                user,
+            }
+            return limits.show(data);
+        }
+        case text.startsWith('change'): {
+            data = {
+                user,
+                channel,
+                who: text.match(/\<@\w{9}\>/) ||  text.match('global'),
+                limit: text.match(/\d+$/),
+            }
+            return limits.change(data);
+        }
+        case text.startsWith('buy'): {
+            data.list = text.split(/(\d+)/).splice(1).reduce((prev, cur) => {
+                const test = Number(cur)
+                if (!Number.isNaN(test)) product.quantity = Number(cur);
+                if (Number.isNaN(test) && product.quantity) product.name = cur.trim();
+                if (product.name && product.quantity) {
+                    prev.push(product)
+                    product = {}
+                    return prev;
+                }
+                return prev;
+            }, []);
+            data.channel = channel;
+            data.user = data.user;
+            return purchase(data);
+        }
+        case /^add product/.test(text): {
+            const info = text.match(/name:((\s)?[\w|\s]+)brand:((\s)?[\w|\s]+)price:(\s\d+)$/);
+            data = {
+                user,
+                channel,
+                name: info[1],
+                brand: info[3],
+                price: info[5],
+            }
+            return products.create(data);
+        }
+        case /^delete product/.test(text): {
+            data = {
+                user,
+                channel,
+            };
+            return products.delete(data);
+        }
+        case /^show debt/.test(text): {
+            data.what = text.split(' ')[2];
+            data.user = user;
+            data.channel = channel;
+            return debt.show(data);
+        }
+        case /pays/.test(text): {
+            const info = text.match(/^<@(.{9})> paid (\d*)/)
+            data.who = info[1];
+            data.amount = info[2];
+            data.user = user;
+            data.channel = channel;
+            return debt.pay(data);
+        }
+        default: {
+            return defaultMessage();
+        }
+    }
 };
 
 module.exports = answerMessage;
--- index/events.js
+++ index/events.js
@@ -2,13 +2,8 @@ const api = require('../events/index');
 
 module.exports = (fastify) => {
   fastify.post('/event', (req, reply) => {
-    const { type } = req.body.event;
     reply.status(200);
     reply.send();
-    switch(type.toLowerCase()) {
-      case 'message': {
-        return api.answerMessage(req.body);
-      }
-    }
+    return api.answerMessage(req.body);
   })
 }
