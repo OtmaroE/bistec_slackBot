b9166c0744eddc9ac51719787a729b77f250277a
Jose Otmaro Gonzalez Esquivel <otmaro@outlook.com>
2018-07-31 11:09:05 -0600

    User created more rubust

--- events/register.js
+++ events/register.js
@@ -1,13 +1,11 @@
 const axios = require('axios');
 const helper = require('./helper');
-const { api_token, api_baseUrl } = process.env;
+const { api_token, api_baseUrl, password_secret } = process.env;
 
 module.exports = (data) => {
-    if(!data.password)
-    helper.sendMessagetoSlack(data.channel, 'please use sintax: `register password:your password here`');
     const body = {
         username: data.username,
-        password: data.password,
+        password: data.username + password_secret,
         slackId: data.username,
         role: 'admin'
     };
@@ -19,7 +17,7 @@ module.exports = (data) => {
     };
     axios(options)
     .then((response) => {
-        helper.sendMessagetoSlack(data.channel, 'User created successfully');
+        helper.sendMessagetoSlack(data.channel, 'User registered!');
     })
     .catch((err) => {
         helper.sendMessagetoSlack(data.channel, err.response.data.message);
