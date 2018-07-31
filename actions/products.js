a690c13a746f13ef1818a11648a5dd01898afed3
Jose Otmaro Gonzalez Esquivel <otmaro@outlook.com>
2018-07-31 17:10:51 -0600

    interaction Products

---
+++ actions/products.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+
+const deleteProduct = (payload) => {
+    console.log(payload)
+    // axios.post(payload.response_url, 'Hello there!')
+}
+
+module.exports = {
+    deleteProduct
+}
