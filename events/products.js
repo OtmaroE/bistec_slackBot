fedd1fe63e23e7520730b15d271b15af9d084aaf
Jose Otmaro Gonzalez Esquivel <otmaro@outlook.com>
2018-07-30 18:43:01 -0600

    add product refactor

--- events/products.js
+++ events/products.js
@@ -1,28 +1,28 @@
 const axios = require('axios');
 const helper = require('./helper');
-const { api_token } = process.env;
+const { api_token, api_baseUrl } = process.env;
 
 const create = (data) => {
     const body = {
         "name": data.name,
         "price": Number(data.price),
         "brand": data.brand
-    }
+    };
     options = {
         method: 'POST',
         headers: { 'Content-Type': 'application/json', 'Authorization': api_token },
         data: body,
-        url: 'http://localhost:3000/api/products'
-    }
+        url: api_baseUrl + '/api/products'
+    };
     axios(options)
     .then((response) => {
         helper.sendMessagetoSlack(data.channel, response.statusText);
     })
     .catch((err) => {
         helper.sendMessagetoSlack(data.channel, err.message);
-    })
+    });
 }
 
 module.exports = {
     create
-}
+};
