b397dfa9351b5e9394744a47f4873441176a2516
Jose Otmaro Gonzalez Esquivel <otmaro@outlook.com>
2018-07-31 11:08:48 -0600

    Implemented user token to request

--- events/products.js
+++ events/products.js
@@ -3,24 +3,27 @@ const helper = require('./helper');
 const { api_token, api_baseUrl } = process.env;
 
 const create = (data) => {
-    const body = {
-        "name": data.name,
-        "price": Number(data.price),
-        "brand": data.brand
-    };
-    options = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json', 'Authorization': api_token },
-        data: body,
-        url: api_baseUrl + '/api/products'
-    };
-    axios(options)
+    helper.logInGetToken(data.user)
+    .then((token) => {
+        const createProductBody = {
+            "name": data.name,
+            "price": Number(data.price),
+            "brand": data.brand
+        };
+        createProductOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json', 'Authorization': token },
+            data: createProductBody,
+            url: api_baseUrl + '/api/products'
+        };
+        return axios(createProductOptions);
+    })
     .then((response) => {
         helper.sendMessagetoSlack(data.channel, response.statusText);
     })
     .catch((err) => {
         helper.sendMessagetoSlack(data.channel, err.message);
-    });
+    })
 }
 
 module.exports = {
