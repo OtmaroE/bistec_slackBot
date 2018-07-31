9bff0effb8ae3a24b85209cc26aebbbfce34ea56
Jose Otmaro Gonzalez Esquivel <otmaro@outlook.com>
2018-07-31 17:10:19 -0600

    Actions endpoint

--- index/events.js
+++ index/events.js
@@ -1,10 +1,14 @@
-const api = require('../events/index');
-
+const events = require('../events');
+const actions = require('../actions');
 module.exports = (fastify) => {
   fastify.post('/event', (req, reply) => {
     reply.status(200);
     if(req.body.event.bot_id) return "";
     reply.send();
-    return api.answerMessage(req.body);
+    return events.answerMessage(req.body);
+  })
+  fastify.post('/actions', (req, reply) => {
+    const payload = JSON.parse(req.body.payload);
+    return actions.answerInteraction(payload);
   })
 }
