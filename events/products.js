d4ec5bb66b20c31a025ff7182a5e351efb8ffd84
Jose Otmaro Gonzalez Esquivel <otmaro@outlook.com>
2018-07-31 17:15:04 -0600

    WIP Delete product

--- events/products.js
+++ events/products.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const helper = require('./helper');
-const { api_token, api_baseUrl } = process.env;
+const { api_token, api_baseUrl, bot_token } = process.env;
+const productList = require('./interactiveJson/productList.json');
 
 const create = (data) => {
     helper.logInGetToken(data.user)
@@ -10,7 +11,7 @@ const create = (data) => {
             "price": Number(data.price),
             "brand": data.brand
         };
-        createProductOptions = {
+        const createProductOptions = {
             method: 'POST',
             headers: { 'Content-Type': 'application/json', 'Authorization': token },
             data: createProductBody,
@@ -24,8 +25,41 @@ const create = (data) => {
     .catch((err) => {
         helper.sendMessagetoSlack(data.channel, err.message);
     })
+};
+const deleteProduct = (data) => {
+    const options = {
+        method: 'GET',
+        headers: { 'Authorization': api_token },
+        url: api_baseUrl + '/api/products'
+    };
+    axios(options)
+    .then((response) => {
+        if(!response.data[0] | !response.data[0].name)
+            Promise.reject('There are not products to delete');
+        let nameArray = response.data[0].name
+        if(response.data.length > 1)
+            nameArray = response.data.map((element) => element.name);
+        for (let name of nameArray){
+            productList[0].actions[0].options.push({ text: name, value: name });
+        }
+        return axios.get('https://slack.com/api/chat.postEphemeral', {
+            params: {
+                    token: bot_token,
+                    channel: data.channel,
+                    text: '',
+                    user: data.user,
+                    attachments: JSON.stringify(productList)
+            }
+        })
+    })
+    .then((response) => {
+        console.log('axios post was received');
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 }
-
 module.exports = {
-    create
+    create,
+    deleteProduct
 };
