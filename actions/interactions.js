4df9ee33ce5d32953e22a5007c70b4473dde7268
Jose Otmaro Gonzalez Esquivel <otmaro@outlook.com>
2018-08-01 10:36:21 -0600

    We receive delete interactions

--- actions/interactions.js
+++ actions/interactions.js
@@ -3,7 +3,10 @@ const product = require('./products');
 const answerInteraction = (payload) => {
     switch (payload.callback_id) {
         case 'product_selection': {
-            return product.deleteProduct(payload)
+            return product.showproducts(payload);
+        }
+        case 'delete': {
+            return product.deleteProduct(payload);
         }
         default: {
             return defaultMessage(payload);
